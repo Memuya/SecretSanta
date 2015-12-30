984f858136a15a93fddcd2f1d2b1a22e152a1e02
Mehmet Uyanik <mehmet.uyanik@live.com.au>
2015-12-30 11:42:23 +1100

    Fixed bug
    Fixed a problem where an empty string values could be passed into an
    array.

--- classes/SecretSanta.php
+++ classes/SecretSanta.php
@@ -135,6 +135,12 @@ class SecretSanta {
 			$str = str_replace($values, ' ', $str);
 			//Turn string into an array
 			$str = explode(' ', strtolower($str));
+
+			//Prevents empty values being passed through to the array
+			for($x = 0; $x < count($str); $x++)
+				if(empty($str[$x]))
+					unset($str[$x]);
+
 			//Makes sure all values inside the array are unquie
 			$str = array_unique($str);
 
